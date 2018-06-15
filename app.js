6dce6b840f31a3bfd7875f9149c864d1b421c7d1
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-15 13:10:54 -0500

    set up

---
+++ app.js
@@ -0,0 +1,12 @@
+// function favMovie(name = 'world',movie = 'the room'){
+//     console.log(`my name is ${name}, and my favorite movie is ${movie}`)
+// };
+
+// favMovie();
+// favMovie('warryn','how to train your dragon');
+
+favMovie((name, movie) => console.log(`My name is ${name} and ${movie} is my favorite movie.`));
+
+getFirstName((first,last) => {
+
+});
